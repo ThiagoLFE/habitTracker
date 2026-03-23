6764ba97a9409b46b1c1c1af7d2bb27d9ffc7216
Thiago Barbosa <thiago.barbosa@digiaccess.com.br>
2026-03-23 10:47:52 -0300

    testes with templ

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	log "log/slog"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("Hello World!")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", HelloWorld)
+	log.Info("Server open on the http://localhost:8000")
+	http.ListenAndServe(":8000", mux)
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	BaseLayout("first page with temple", HeaderPage("Hello World")).Render(r.Context(), w)
 }
